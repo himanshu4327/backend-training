a34b0ab64b9d986921d48bf75021becfd285abfa
himanshu <himanshusoni15695@gmail.com>
2022-08-09 20:08:17 +0530

    solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,17 +7,78 @@ const stringchange = require('../validator/formatter')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send("welcome to my application. my name is himanshu soni and i am part of FunctionUp Plutonium cohort");
-    firstModule.welcome();
-    firstHelper.date();
-    firstHelper.month();
-    firstHelper.batch();
-    stringchange.trim();
-    stringchange.lower();
-    stringchange.upper();
-
+router.get('/movies', function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies);
 });
+
+
+
+router.get("/movies/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    let ourmovie = movies[movieIndex]
+    res.send(ourmovie)
+});
+
+
+router.get("/movies/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+
+    if (movieIndex < 0 || movieIndex >= movies.length) {
+        return req.send('invalid input try again')
+    }
+    let ourmovie = movies[movieIndex]
+    res.send(ourmovie)
+});
+
+router.get("/films", function (req, res) {
+    [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+    res.send(films)
+
+})
+
+
+router.get("/films/:filmId", function (req, res) {
+    [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+
+    let filmId = req.params.filmId
+    for (let i = 0; i < films.length; i++) {
+        let film = films[i]
+        if (film.id == filmId) {
+            return res.send(film)
+        }
+    }
+    res.send("invaild film id")
+})
+
 
 module.exports = router;
 // adding this comment for no reason
